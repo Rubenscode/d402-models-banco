3fd111f1f2f03fad1a2a6b191908702a3b55d76e
Rubens Almeida de Andrade <rubens.andrade1@gmail.com>
2019-07-25 11:59:33 -0300

    .

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 # Create your views here.
 
-def mostrar_hello(request):
+
+# def mostrar_hello(request):
+#     return render(request, 'index.html')
+
+
+def mostrar_formulario_cadastro(request):
+
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST['nome']
+        pessoa.cpf = request.POST['cpf']
+        pessoa.save()
+
     return render(request, 'index.html')
+
+def mostrar_pessoas(request):
+    pessoas = Pessoa.objects.all()
+
+    return render(request, 'pessoas.html')
